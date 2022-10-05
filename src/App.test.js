90c4ed3a749739e0d3036b0b5bbd014a386e4229
robin-lillqvist <robin.lillqvist@gmail.com>
2022-10-05 09:59:25 +0200

    installed sass

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Beer Bros Vodcast/i);
   expect(linkElement).toBeInTheDocument();
 });
