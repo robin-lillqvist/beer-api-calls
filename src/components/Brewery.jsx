470abdd0a6ab49240edf382b601e167828141732
Robin Lillqvist <robin.lillqvist@comeon.com>
2022-10-27 10:58:59 +0200

    made comparison safer

--- src/components/Brewery.jsx
+++ src/components/Brewery.jsx
@@ -3,7 +3,7 @@ import BasicCard from "./Card";
 const Brewery = (breweryData) => {
   const sortedBreweryData = breweryData.data
     .sort(function (a, b) {
-      return a.brewery_type.localeCompare(b.brewery_type) || b.name - a.name;
+      return a.brewery_type.localeCompare(b.brewery_type) || a.name.localeCompare(b.name);
     })
     .map((brewery, index) => {
       return <BasicCard data={brewery} key={index} />;
