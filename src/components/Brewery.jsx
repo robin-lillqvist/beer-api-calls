9867efec20d972d5dd5cd9b6902c56849f636e0e
Robin Lillqvist <robin.lillqvist@comeon.com>
2022-10-27 10:53:40 +0200

    multiple stage sorting

--- src/components/Brewery.jsx
+++ src/components/Brewery.jsx
@@ -1,16 +1,19 @@
 import BasicCard from "./Card";
 
 const Brewery = (breweryData) => {
-  const myData = []
-    .concat(breweryData.data)
-    .sort((a, b) => a.name > b.name ? 1 : -1)
+  const newSort = breweryData.data
+    .sort(function (a, b) {
+      return a.brewery_type.localeCompare(b.brewery_type) || b.name - a.name;
+    })
     .map((brewery, index) => {
       return <BasicCard data={brewery} key={index} />;
     });
 
+  console.log(newSort);
+
   return (
     <div className="brewery-list" key="BreweryResults">
-      {myData}
+      {newSort}
     </div>
   );
 };
