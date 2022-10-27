682c44558a22d5ef7445aa0df42f9ee236074ea2
Robin Lillqvist <robin.lillqvist@comeon.com>
2022-10-27 10:54:29 +0200

    rename variable

--- src/components/Brewery.jsx
+++ src/components/Brewery.jsx
@@ -1,7 +1,7 @@
 import BasicCard from "./Card";
 
 const Brewery = (breweryData) => {
-  const newSort = breweryData.data
+  const sortedBreweryData = breweryData.data
     .sort(function (a, b) {
       return a.brewery_type.localeCompare(b.brewery_type) || b.name - a.name;
     })
@@ -9,11 +9,9 @@ const Brewery = (breweryData) => {
       return <BasicCard data={brewery} key={index} />;
     });
 
-  console.log(newSort);
-
   return (
     <div className="brewery-list" key="BreweryResults">
-      {newSort}
+      {sortedBreweryData}
     </div>
   );
 };
